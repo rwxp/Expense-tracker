42a39fda799d2ae484b102187556ea6ea24e1706
Sebastian Caicedo <sdrivert@hotmail.com>
2025-04-25 15:04:13 -0500

    Agrega capa de persistencia con JPA

--- src/main/java/com/dev/personalfinance/domain/models/Expense.java
+++ src/main/java/com/dev/personalfinance/domain/models/Expense.java
@@ -1,34 +1,17 @@
-
 package com.dev.personalfinance.domain.models;
 
-import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
-@Table(name = "expenses")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class Expense {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false)
     private String title;
-
-    @Column(nullable = false)
     private Long amount;
-
-    @Column(nullable = false)
     private String date;
-
-    @Column
     private String description;
-
-    @Column
     private String category;
-
 }
